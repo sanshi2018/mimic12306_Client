b49e4c3abb5e99d0a84f5f4feb2b12b7122d4b44
sanshi <49914390+sanshi2018@users.noreply.github.com>
2024-01-03 23:20:38 +0800

    9.13 制作车次下拉框组件

--- admin/src/views/main/TrainStationView.vue
+++ admin/src/views/main/TrainStationView.vue
@@ -28,7 +28,12 @@
            ok-text="确认" cancel-text="取消">
     <a-form :model="trainStation" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
       <a-form-item label="车次编号">
-        <a-input v-model:value="trainStation.trainCode" />
+<!--        <a-select v-model:value="trainStation.trainCode" show-search:filterOption="filterTrainCodeOption">-->
+<!--          <a-select-option v-for="item in trains" :key="item.code" :value="item.code" :lable="item.code + item.start + item.end">-->
+<!--          {{item.code}} | {{item.start}} ~ {{item.end}}-->
+<!--          </a-select-option>-->
+<!--        </a-select>-->
+        <train-select-view v-model="trainStation.trainCode" />
       </a-form-item>
       <a-form-item label="站序">
         <a-input v-model:value="trainStation.index" />
@@ -60,9 +65,11 @@ import { defineComponent, ref, onMounted, watch } from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
 import { pinyin } from 'pinyin-pro';
+import TrainSelectView from "@/components/train-select.vue";
 
 export default defineComponent({
   name: "train-station-view",
+  components: {TrainSelectView},
   setup() {
     const visible = ref(false);
     let trainStation = ref({
@@ -219,15 +226,29 @@ export default defineComponent({
       });
     };
 
+    // ----------车次下拉框---------------
+    const trains = ref([]);
+
+    /**
+     * 查询所有车次,用于下拉框数据
+     */
     const queryTrainCode = () => {
       axios.get("/business/admin/train/query-all").then((response) => {
         let data = response.data
         if(data.success) {
-          console.log(data.content);
+          trains.value = data.content
         }else {
           notification.error({description: data.message})
         }
       })
+    }
+
+    /**
+     * 车次下拉框选项
+     */
+    const filterTrainCodeOption = (input, option) => {
+      console.log(input, option)
+      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
     }
 
     onMounted(() => {
@@ -235,7 +256,7 @@ export default defineComponent({
         page: 1,
         size: pagination.value.pageSize
       });
-      
+
       queryTrainCode();
     });
 
@@ -251,7 +272,9 @@ export default defineComponent({
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
+      trains,
+      filterTrainCodeOption
     };
   },
 });
